db02f0033357ab58f124646b4337920117f515df
Folke Schönberg <folke.schonberg@elev.ga.lbs.se>
2024-04-20 17:00:40 +0200

    first commit

--- Main-scripts/main-imgSlider.js
+++ Main-scripts/main-imgSlider.js
@@ -46,7 +46,7 @@ let ULcount = 1;
 SclickArea.addEventListener("click", function(event) {
 
   if (event.target.tagName != "LI"){
-    if (sliderUL.children.length < 6 && window.innerWidth < 700) {
+    if (sliderUL.children.length < 60 && window.innerWidth > 700) {
 
       tempIMG = document.createElement("img");
       tempIMG.className = "img-Slider-li-img";
@@ -69,46 +69,3 @@ SclickArea.addEventListener("click", function(event) {
     sliderUL.removeChild(event.target);
   }
 });
-
-
-const container = document.querySelector('#img-Slider-ul');
-                
-let startY;
-let startX;
-let scrollLeft;
-let scrollTop;
-let isDown;
-
-container.addEventListener('mousedown',e => mouseIsDown(e));  
-container.addEventListener('mouseup',e => mouseUp(e))
-container.addEventListener('mouseleave',e=>mouseLeave(e));
-container.addEventListener('mousemove',e=>mouseMove(e));
-
-function mouseIsDown(e){
-  isDown = true;
-  startY = e.pageY - container.offsetTop;
-  startX = e.pageX - container.offsetLeft;
-  scrollLeft = container.scrollLeft;
-  scrollTop = container.scrollTop; 
-}
-function mouseUp(e){
-  isDown = false;
-}
-function mouseLeave(e){
-  isDown = false;
-}
-function mouseMove(e){
-  if(isDown){
-    e.preventDefault();
-    //Move vertcally
-    const y = e.pageY - container.offsetTop;
-    const walkY = y - startY;
-    container.scrollTop = scrollTop - walkY;
-
-    //Move Horizontally
-    const x = e.pageX - container.offsetLeft;
-    const walkX = x - startX;
-    container.scrollLeft = scrollLeft - walkX;
-
-  }
-}
