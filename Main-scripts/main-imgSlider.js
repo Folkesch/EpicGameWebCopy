5d18a9047ae12f6bbf464f6582061bea931c1dd9
Folke Schönberg <folke.schonberg@elev.ga.lbs.se>
2024-04-20 15:02:07 +0200

    first commit

--- Main-scripts/main-imgSlider.js
+++ Main-scripts/main-imgSlider.js
@@ -4,6 +4,33 @@ const SclickArea = document.getElementById("img-Slider-rightSection");
 var pageTitle = document.title;
 var attentionMessage = 'Come Back!';
 
+window.addEventListener("load", function() {
+  if (window.innerWidth < 700) {
+    imgs = document.getElementsByClassName("img-Slider-li-img");
+
+    for (var i = 0; i < imgs.length; i++) {
+      imgs.item(i).src = "img/main/en-epic-savings-april-carousel-mobile-10b6385734ea.avif";
+    }
+  }
+});
+
+window.addEventListener("resize", function() {
+  if (window.innerWidth < 700) {
+    imgs = document.getElementsByClassName("img-Slider-li-img");
+
+    for (var i = 0; i < imgs.length; i++) {
+      imgs.item(i).src = "img/main/en-epic-savings-april-carousel-mobile-10b6385734ea.avif";
+    }
+  }
+  else {
+    imgs = document.getElementsByClassName("img-Slider-li-img");
+
+    for (var i = 0; i < imgs.length; i++) {
+      imgs.item(i).src = "img/main/en-epic-savings-april-carousel-thumbnail-1200x1600-6df4d517f365.avif";
+    }
+  }
+})
+
 document.addEventListener('visibilitychange', function(e) {
   var isPageActive = !document.hidden;
 
@@ -18,12 +45,12 @@ let ULcount = 1;
 
 SclickArea.addEventListener("click", function(event) {
 
-  if (event.target.tagName != "LI"/* && event.target.className != "img-Slider-li-p" && event.target.className != "img-Slider-li-img"*/){
-    if (sliderUL.children.length < 60) {
+  if (event.target.tagName != "LI"){
+    if (sliderUL.children.length < 6 && window.innerWidth < 700) {
 
       tempIMG = document.createElement("img");
       tempIMG.className = "img-Slider-li-img";
-      tempIMG.src = "https://cdn2.unrealengine.com/en-epic-savings-april-carousel-thumbnail-1200x1600-6df4d517f365.jpg?h=128&quality=medium&resize=1&w=96";
+      tempIMG.src = "img/main/en-epic-savings-april-carousel-thumbnail-1200x1600-6df4d517f365.avif";
 
       tempH5 = document.createElement("h5");
       tempH5.className = "img-Slider-li-p";
@@ -37,9 +64,51 @@ SclickArea.addEventListener("click", function(event) {
 
       sliderUL.appendChild(tempLI);
     }
-    return;
   }
   else {
     sliderUL.removeChild(event.target);
   }
 });
+
+
+const container = document.querySelector('#img-Slider-ul');
+                
+let startY;
+let startX;
+let scrollLeft;
+let scrollTop;
+let isDown;
+
+container.addEventListener('mousedown',e => mouseIsDown(e));  
+container.addEventListener('mouseup',e => mouseUp(e))
+container.addEventListener('mouseleave',e=>mouseLeave(e));
+container.addEventListener('mousemove',e=>mouseMove(e));
+
+function mouseIsDown(e){
+  isDown = true;
+  startY = e.pageY - container.offsetTop;
+  startX = e.pageX - container.offsetLeft;
+  scrollLeft = container.scrollLeft;
+  scrollTop = container.scrollTop; 
+}
+function mouseUp(e){
+  isDown = false;
+}
+function mouseLeave(e){
+  isDown = false;
+}
+function mouseMove(e){
+  if(isDown){
+    e.preventDefault();
+    //Move vertcally
+    const y = e.pageY - container.offsetTop;
+    const walkY = y - startY;
+    container.scrollTop = scrollTop - walkY;
+
+    //Move Horizontally
+    const x = e.pageX - container.offsetLeft;
+    const walkX = x - startX;
+    container.scrollLeft = scrollLeft - walkX;
+
+  }
+}
